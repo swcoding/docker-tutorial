32b7aed838e87ecc31b39ce01d05bdc1a0edcc1d
Jeffery Wang <jeffery.wang@mobagel.com>
2022-09-23 16:02:41 +0800

    add init db

--- lesson1/backend/app.py
+++ lesson1/backend/app.py
@@ -1,8 +1,37 @@
+from typing import Optional
+
+from beanie import Document, init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import FastAPI
 
-app = FastAPI()
 
+class Greeting(Document):
+    message: str
+
+
+async def init():
+    # Create Motor client
+    client = AsyncIOMotorClient(
+        "mongodb://mongo-admin:mongo-password@192.168.6.168:27017"
+    )
+
+    # Initialize beanie with the Product document class and a database
+    await init_beanie(database=client.backend, document_models=[Greeting])
+
+
+app = FastAPI(on_startup=[init])
 
 @app.get("/")
-async def root(name: str = "nononononono"):
-    return {"message": f"Hello {name}"}
+async def hello(name: str = "hello world"):
+    return name
+
+@app.post("/")
+async def create(name: str = "world"):
+    message = Greeting(message=f"Hello {name}!")
+    await message.save()
+    return message
+
+@app.get("/all")
+async def get_all():
+    greetings = await Greeting.find().to_list()
+    return greetings
